45063e4c42688a16c205ab2dde65ea689cb640ac
Tauri Kirsipuu <tauri139@users.noreply.github.com>
2016-02-03 10:40:25 +0200

    inch to cm calculator

---
+++ calc.js
@@ -0,0 +1,18 @@
+//ootame kun leht on laetud
+
+window.onload = function(){
+
+  // saan kätte väljad
+  var cm = document.getElementById('cm');
+  var inch = document.getElementById('inch');
+
+  //Lisan nuppudele kuularid
+  document.getElementById('to_inch').addEventListener('click', convertToInch);
+  document.getElementById('to_cm').addEventListener('click', function(){
+    cm.value = inch.value*2.54;
+  });
+};
+
+function convertToInch(){
+  inch.value = cm.value/2.54;
+}
